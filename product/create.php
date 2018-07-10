ceb0cef0bee5b6c3fe2ceba5c41274058dab97ae
zohzik <taymyr1026@gmail.com>
2018-07-09 21:44:03 -0500

    added create product

---
+++ product/create.php
@@ -0,0 +1,39 @@
+<?php
+//required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers; Content-Type, Access-Control-Allow-Headers, 
+	Authorization, X-Requested-With");
+	
+//get database connection
+include_once '../config/database.php';
+//instantiate product object
+include_once '../objects/product.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$product = new Product($db);
+
+//get posted data
+$data = json_decode(file_get_contents("php://input"));
+
+//set product property values
+$product->name = $data->name;
+$prodcut->price = $data->price;
+$product->description = $data->description;
+$product->category_id = $data->category_id;
+$product->created = date('Y-m-d H:i:s');
+
+//create the product
+if($product->create()){
+	echo '{';
+		echo '"message": "Product was created."';
+	echo '}';
+}else{
+	echo '{';
+		echo '"message": "Product was not created."';
+	echo '}';
+}
+?>

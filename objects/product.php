8d07833055a30f4bd3c0303fa0ee38785ea9300d
zohzik <taymyr1026@gmail.com>
2018-07-22 23:49:49 -0500

    added read once

--- objects/product.php
+++ objects/product.php
@@ -73,5 +73,39 @@ class Product{
 		
 		return false;
 	}
+	
+	//used when filling up the update product form
+	function readOne(){
+		//query to read single record
+		$query = "SELECT
+			c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created
+			FROM " . $this->table_name . " p
+			LEFT JOIN
+				categories c
+					ON p.category_id = c.id
+			WHERE
+				p.id = ?
+			LIMIT
+				0,1";
+				
+		//prepare query statement
+		$stmt = $this->conn->prepare($query);
+		
+		//bind id of product to be updated
+		$stmt->bindParam(1, $this->id);
+		
+		//execute query
+		$stmt->execute();
+		
+		//get retrieved row
+		$row = $stmt->fetch(PDO::FETCH_ASSOC);
+		
+		//set values to object properties
+		$this->name = $row['name'];
+		$this->price = $row['price'];
+		$this->description = $row['description'];
+		$this->category_id = $row['category_id'];
+		$this->category_name = $row['category_name'];
+	}
 }
 ?>
